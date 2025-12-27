7ed509bde38829475716971f05f2237e35c81bda
Jordy <jsmall53@ymail.com>
2025-12-27 08:49:57 -0800

    working on graph solution

--- day_3/lobby.c
+++ day_3/lobby.c
@@ -90,6 +90,129 @@ uint32_t get_max_joltage(const char* bank, digit_table_t* table) {
     return digit_table_get_max(table);
 }
 
+typedef struct graph_node graph_node_t;
+typedef struct graph graph_t;
+
+typedef struct graph_node {
+    const char* c;
+    graph_node_t** edges;
+    size_t num_edges;
+} graph_node_t;
+
+typedef struct graph {
+    graph_node_t* nodes;
+    size_t len;
+    size_t capacity;
+} graph_t;
+
+graph_t* graph_create(size_t max_nodes) {
+    graph_t* graph = (graph_t*)malloc(sizeof(graph_t));
+    graph->nodes = (graph_node_t*)malloc(sizeof(graph_node_t) * max_nodes);
+    graph->capacity = max_nodes;
+    graph->len = 0;
+    return graph;
+}
+
+graph_node_t* graph_add_node(graph_t* graph, const char* ptr) {
+    graph->nodes[graph->len].c = ptr;
+    graph->nodes[graph->len].edges = (graph_node_t**)malloc(sizeof(graph_node_t*) * graph->capacity);
+    graph->nodes[graph->len].num_edges = 0;
+    return &graph->nodes[graph->len++];
+}
+
+void graph_add_edge(graph_node_t* from, graph_node_t* to) {
+    from->edges[from->num_edges++] = to;
+}
+
+uint64_t graph_node_value(graph_node_t* node) {
+    return (*node->c) - '0';
+}
+
+uint64_t graph_dfs(graph_node_t* node, size_t depth, size_t max_depth) {
+    if (node == NULL) {
+        return 0; // return what?
+    }
+
+    printf("searching %x\n", node);
+    if (node->num_edges == 0 || depth == max_depth) {
+        return graph_node_value(node);
+    }
+
+    uint64_t max_val = 0;
+    uint64_t node_val = graph_node_value(node);
+    for (size_t i = 0; i < node->num_edges; i++) {
+        uint64_t val = graph_dfs(node->edges[i], depth + 1, max_depth);
+        val = node_val * 10 + val;
+        printf("%zu\n", val);
+        if (val > max_val)
+            max_val = val;
+    }
+    return max_val;
+}
+
+// Use DFS to generate all possible n digit values from the graph.
+void graph_calculate_max(graph_t* graph, int max_digits) {
+
+}
+
+
+// This is broken for some reason.
+// graph_node_t* graph_find_node(graph_t* graph, const char* val) {
+//     for (size_t i; i < graph->len; i++) {
+//         if (val == graph->nodes[i].c)
+//             return &graph->nodes[i];
+//     }
+//     return NULL;
+// }
+
+void test_graph() {
+    const char* str = "123456789";
+    int len = strlen(str);
+    graph_t* graph = graph_create(len);
+    for (size_t i = 0; i < len; i++) {
+        graph_add_node(graph, str + i);
+        assert(graph->len == i + 1);
+    }
+
+    for (size_t i = 0; i < graph->len; i++) {
+        graph_node_t* node = &graph->nodes[i];
+        const char* val = str + i;
+        assert(node->c == val);
+        // graph_node_t* found = graph_find_node(graph, val);
+        // printf("%x %x\n", node, found);
+        // assert(node == found);
+        for (size_t j = i + 1; j < graph->len; j++) {
+            graph_node_t* to_node = &graph->nodes[j];
+            graph_add_edge(node, to_node);
+        }
+        assert(node->num_edges == len - i - 1);
+        printf("(%x) %s (%zu)\n", node, node->c, node->num_edges);
+    }
+
+    uint64_t val = graph_dfs(&graph->nodes[0], 0, 8);
+    printf("MAX VALUE: %zu\n", val);
+
+
+    // for (size_t i = 0; i < len; i++) {
+    //     graph_node_t* node = graph_find_node(graph, str + i);
+    //     assert(node != NULL);
+    //
+    //     for (size_t j = j + 1; j < len; j++) {
+    //         // TODO: add edges
+    //         graph_node_t* to_node = graph_find_node(graph, str + j);
+    //         graph_add_edge(node, to_node);
+    //     }
+    //     printf("(%x) %s (%zu)\n", node, node->c, node->num_edges);
+    // }
+
+    // for (size_t i = 0; i < len; i++) {
+    //     graph_node_t* node = graph_find_node(graph, str + i);
+    //     assert(node != NULL);
+    // }
+
+    printf("test_graph passed.\n");
+}
+
 void test_table() {
     digit_table_t* table = digit_table_create();
     assert(digit_table_get_value(table, 1) == -1);
@@ -117,9 +240,13 @@ void test_bank() {
     printf("test_bank passed.\n");
 }
 
+/*
+ * Part 2 is likely a tree or DAG.
+ * */
 int main() {
     test_table();
     test_bank();
+    test_graph();
     FILE* file = fopen("input.txt", "r");
     if (file == NULL) {
         printf("FAILED TO READ INPUT FILE.");

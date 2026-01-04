dca6bc3ab070856072521ed53f610f544a75f7a5
Jordy <jsmall53@ymail.com>
2026-01-04 08:50:24 -0800

    setup dir for day 7

---
+++ day_7/beam.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+#include <assert.h>
+#include <ctype.h>
+
+void part1() {
+
+}
+
+int main() {
+
+}

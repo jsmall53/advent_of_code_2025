512df8502bd8f174a9f790c188a63de56ad0b730
Jordy <jsmall53@ymail.com>
2026-01-04 08:52:48 -0800

    fixing branches

--- day_7/beam.c
+++ day_7/beam.c
@@ -6,10 +6,20 @@
 #include <assert.h>
 #include <ctype.h>
 
+void part1_read_file(const char* filename) {
+    FILE* file = fopen(filename, "r");
+    if (!file) {
+        printf("FAILED TO READ INPUT FILE\n");
+        exit(1);
+    }
+
+    fclose(file);
+}
 void part1() {
+    part1_read_file("sample.txt");
 
 }
 
 int main() {
-
+    part1();
 }

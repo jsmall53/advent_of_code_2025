b7b72e515044b9699d9fdf8e3e81b12553b1386d
Jordy <jsmall53@ymail.com>
2026-01-24 07:41:58 -0800

    complete day 11 solution

--- day_11/reactor.c
+++ day_11/reactor.c
@@ -49,6 +49,7 @@ static connections_t connections[MAX_DEVICES];
 void reset_devices() {
     memset(devices, 0, sizeof(id_string_t) * MAX_DEVICES);
     n_devices = 0;
+    memset(connections, 0, sizeof(connections_t) * MAX_DEVICES);
 }
 
 int find_id(const char* tok) {
@@ -106,7 +107,6 @@ void read_file(const char* filename) {
 
     fseek(file, 0, SEEK_SET);
     memset(buf, 0, BUF_SIZE);
-    int n_outlets = 0;
     while (fgets(buf, BUF_SIZE, file)) {
         const char* ptr = buf;
         int device_id = find_id(buf);
@@ -120,9 +120,6 @@ void read_file(const char* filename) {
                 int id = find_id(ptr);
                 assert(id < MAX_DEVICES && id > -1);
                 connections_push(&connections[device_id], id);
-                if (id == 0) {
-                    n_outlets++;
-                }
                 while (isalpha(*ptr)) ptr++;
             } else {
                 printf("Unhandled token '%c'\n", *ptr);
@@ -130,7 +127,6 @@ void read_file(const char* filename) {
             }
         }
     }
-    printf("found %i outlets\n", n_outlets);
 }
 
 void connections_debug_print(connections_t* conn) {
@@ -140,52 +136,170 @@ void connections_debug_print(connections_t* conn) {
     printf("\n");
 }
 
-int count_paths(int id, int* visited, int* memo) {
-    if (id == 0) // found outlet
+int count_paths_dfs(int id, int dst, int* visited, int* reachable) {
+    if (id == dst) {// found outlet
         return 1;
-
-    if (memo[id] != 0) {
-        return memo[id];
-    }
-
-    if (visited[id] != 0) { // avoid cycles
-        // printf("Found cycle %s\n", devices[id].str);
-        return 0;
     }
     visited[id] = 1;
-
     connections_t* conns = &connections[id];
-    assert(conns != NULL && conns->list != NULL);
     int count = 0;
-    for (int i = 0; i < conns->len; i++) {
-        count += count_paths(conns->list[i], visited, memo);
+    if (conns->len > 0) {
+        for (int i = 0; i < conns->len; i++) {
+            int next_id = conns->list[i];
+            if (visited[next_id] == 0 && reachable[next_id]) {
+                count += count_paths_dfs(next_id, dst, visited, reachable);
+            }
+        }
     }
-    // update the memoization table and remove the node from
-    // the current path.
-    memo[id] = count;
+    // remove the node from the current path.
     visited[id] = 0;
     return count;
 }
+
+
+void path_debug_print(int* visited) {
+    for (int i = 0; i < MAX_DEVICES; i++) {
+        if (visited[i] == 1) {
+            printf("%s,", devices[i].str);
+        }
+    }
+    printf("\n");
+}
+
+int can_reach(int id, int dst, int* visited) {
+    if (id == dst) {
+        return 1;
+    }
+
+    if (visited[id] != 0) {
+        return 0;
+    }
+
+    visited[id] = 1;
+    connections_t* conns = &connections[id];
+    int count = 0;
+    for (int i = 0; i < conns->len; i++) {
+        if (can_reach(conns->list[i], dst, visited) > 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void find_reachable(int id, int dst, int* visited, int* reachable) {
+    if (id == dst) {
+        reachable[id] = 1;
+        return;
+    }
+
+    if (reachable[id] == 1)
+        return;
+
+    int visited2[MAX_DEVICES] = {0};
+    reachable[id] = can_reach(id, dst, visited2);
+    visited[id] = 1;
+    connections_t* conns = &connections[id];
+    int count = 0;
+    for (int i = 0; i < conns->len; i++) {
+        if (visited[conns->list[i]] == 0) {
+            find_reachable(conns->list[i], dst, visited, reachable);
+        }
+    }
+    return;
+
+}
+
+void visited_dbg_print(int* visited) {
+    int count = 0;
+    for (int i = 0; i < MAX_DEVICES; i++) {
+        if (visited[i] == 1)
+            printf("%s,", devices[i].str);
+        count += visited[i];
+    }
+    printf(" visited %i vertices\n", count);
+}
+
 
 int part1(const char* filename) {
     reset_devices();
     read_file(filename);
     assert(find_id("you") > 0);
-    int memo[MAX_DEVICES];
-    int visited[MAX_DEVICES];
-    memset(memo, 0, MAX_DEVICES * sizeof(int));
+    int reachable[MAX_DEVICES] = {0};
+    int visited[MAX_DEVICES] = {0};
+    find_reachable(find_id("you"), find_id("out"), visited, reachable);
+    // visited_dbg_print(reachable);
+    // memset(reachable, 1, MAX_DEVICES * sizeof(int));
     memset(visited, 0, MAX_DEVICES * sizeof(int));
-    return count_paths(find_id("you"), visited, memo);
+    return count_paths_dfs(find_id("you"), find_id("out"), visited, reachable);
 }
 
+void explore(int id, int* visited) {
+    visited[id] = 1;
+    connections_t* conns = &connections[id];
+    for (int i = 0; i < conns->len; i++) {
+        if (visited[i] == 0)
+            explore(conns->list[i], visited);
+    }
+    return;
+}
+
+// 150 too low
+// 5392 too low
+// 10006182 too low
+size_t part2(const char* filename) {
+    reset_devices();
+    read_file(filename);
+    assert(find_id("svr") > 0 && find_id("dac") > 0 && find_id("fft") > 0);
+
+    int svr = find_id("svr");
+    int fft = find_id("fft");
+    int dac = find_id("dac");
+    int out = 0;
+
+    int visited[MAX_DEVICES] = {0};
+    assert(!can_reach(dac, fft, visited));
+    memset(visited, 0, sizeof(int) * MAX_DEVICES);
+
+    int reachable_set[MAX_DEVICES] = {0};
+    size_t total = 1;
+
+    /* Addition of each path segment didn't work 
+     * Its multiplication, duh.*/
+    find_reachable(svr, fft, visited, reachable_set);
+    memset(visited, 0, sizeof(int) * MAX_DEVICES);
+    total *= count_paths_dfs(svr, fft, visited, reachable_set);
+    printf("svr->fft: %zu\n", total);
+
+    memset(reachable_set, 0, sizeof(int) * MAX_DEVICES);
+    memset(visited, 0, sizeof(int) * MAX_DEVICES);
+    find_reachable(fft, dac, visited, reachable_set);
+    memset(visited, 0, sizeof(int) * MAX_DEVICES);
+    total *= count_paths_dfs(fft, dac, visited, reachable_set);
+    printf("svr->fft->dac: %zu\n", total);
+
+    memset(reachable_set, 0, sizeof(int) * MAX_DEVICES);
+    memset(visited, 0, sizeof(int) * MAX_DEVICES);
+    find_reachable(dac, out, visited, reachable_set);
+    memset(visited, 0, sizeof(int) * MAX_DEVICES);
+    total *= count_paths_dfs(find_id("dac"), find_id("out"), visited, reachable_set);
+    printf("svr->fft->dac->out: %zu\n", total);
+
+    return total;
+}
+
+
+
 void run_file(const char* filename) {
-    printf("Running %s\n", filename);
+    printf("\nRunning %s\n", filename);
     int n_paths = part1(filename);
     printf("Part 1: %i\n", n_paths);
+    size_t paths = part2(filename);
+    printf("Part 2: %zu\n", paths);
 }
 
 int main() {
-    run_file("sample.txt");
-    // 1875635490 too high...
+    part1("sample.txt");
+    printf("Sample part 2: %zu\n", part2("sample2.txt"));
+
     run_file("input.txt");
 }
